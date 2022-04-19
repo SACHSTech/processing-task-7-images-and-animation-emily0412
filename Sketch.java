1a5c31c26f6f44e92a39e55e7337a5b23d1ffab3
emily0412 <emily.ma23@ycdsbk12.ca>
2022-04-19 18:43:46 -0400

    final

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,26 @@
 import processing.core.PApplet;
+import processing.core.PImage; // import the PImage library
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+	PImage imgSpace; // declare a global image variable
+  PImage imgUFO;
+  PImage imgPlanet;
+
+  float ufoX = 200;
+  float ufoY = 100;
+
+  float moveVariableX = 2;
+  float moveVariableY = 2;
+
+  float circleX = 0;
+  float circleY = 100;
+  float circleAngle = 0;
+
   public void settings() {
-	// put your size call here
-    size(400, 400);
+	
+    size(825, 600);
+    
   }
 
   /** 
@@ -16,20 +28,46 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    imgSpace = loadImage("spaceBack.jpg");
+    imgSpace.resize(imgSpace.width/2, imgSpace.height/2); // resize background
+
+    imgUFO = loadImage("ufo.png");
+    imgUFO.resize(imgUFO.width/6, imgUFO.height/6); //resize UFO
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+  
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    image(imgSpace, 0, 0);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // planet
+    fill(255, 191, 179);
+    noStroke();
+    ellipse(circleX, circleY, 100, 100);
+    circleX = circleX += 1;
+    circleY = circleY + sin(circleAngle)*2; // wave form
+    circleAngle += PI/120;
+    // edge detection
+    if(circleX < 0 || circleX > width) {
+      circleX = 0;
+    }
+    if(circleY < 0 || circleY > height) {
+      circleY = 100;
+    }
+
+    // ufo
+    image(imgUFO, ufoX, ufoY);
+    ufoX += moveVariableX;
+    ufoY += moveVariableY;
+    //bounce off left and right
+    if(ufoX < 0 || ufoX > width - 200) {
+      moveVariableX = moveVariableX * -1;
+    }
+    // bounce off top and bottom
+    if(ufoY < -15 || ufoY > height - 150) {
+      moveVariableY = moveVariableY * -1;
+    }
+
   }
   
   // define other methods down here.
